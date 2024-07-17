38892ef3687b66c11d76a755bd0b080e341c50c0
christianhuamanlostaunau <dread_marley@hotmail.com>
2024-07-17 13:17:34 -0500

    fix: script SQL

--- EXAMEN_APIREST/ScriptSql/init.sql
+++ EXAMEN_APIREST/ScriptSql/init.sql
@@ -30,14 +30,14 @@ VALUES
 ('Elvira Arbulu', '62548758', 5000, 30, 'Jefe de Calidad', '2021-07-07'),
 ('Enrique Ortega', '65475315', 6000, 34, 'Lider Tecnico', '2021-08-13'),
 -- 5 del 2021
-('Nancy White', '45678901', 48000, 26, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 24, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 23, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 26, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 22, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 21, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 29, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 24, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 21, 'Digitador', '2022-04-12'),
-('Nancy White', '45678901', 48000, 22, 'Digitador', '2022-04-12');
+('Nancy Hernades', '45678901', 48000, 26, 'Digitador', '2022-04-12'),
+('Pedro Paredes', '24587546', 48000, 24, 'Digitador', '2022-05-09'),
+('Julio Menendez', '65485321', 48000, 23, 'Digitador', '2022-10-12'),
+('Albaro Paes', '98564875', 48000, 26, 'Digitador', '2022-08-21'),
+('Andrea Estrada', '89653458', 48000, 22, 'Digitador', '2022-07-15'),
+('Lucia Nano', '44527866', 48000, 21, 'Digitador', '2022-04-19'),
+('Paty Luna', '66587912', 48000, 29, 'Digitador', '2022-11-11'),
+('Elsa Danez', '99445876', 48000, 24, 'Digitador', '2022-04-10'),
+('Alejandra Meza', '32664225', 48000, 21, 'Digitador', '2022-12-12'),
+('David Marley', '15429876', 48000, 22, 'Digitador', '2022-01-02');
 -- 10 del 2022
